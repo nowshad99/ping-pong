e45470d6fb33b9997a4062b1ce2ef68509a8ad7b
nowshad99 <nowshad.ahmed@sainsburys.co.uk>
2023-03-30 12:42:48 +0100

    feat: add placeholder score

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ let ballX = 250;
 let ballY = 350;
 const ballRadius = 5;
 
+// Score
+let playerScore = 0;
+let computerScore = 0;
+
 // Render everything on Canvas
 function renderCanvas() {
   // Canvas Background
@@ -37,14 +41,19 @@ function renderCanvas() {
   context.setLineDash([4]);
   context.moveTo(0, 350);
   context.lineTo(500, 350);
-  context.strokeStyle = 'grey';
+  context.strokeStyle = "grey";
   context.stroke();
 
   // Ball
   context.beginPath();
   context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
   context.fill();
+
+  // Score
+  context.font = "32px Courier New";
+  context.fillText(playerScore, 20, canvas.height / 2 + 50);
+  context.fillText(computerScore, 20, canvas.height / 2 - 30);
 }
 
 // Create Canvas Element
