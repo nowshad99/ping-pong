7253d44cf2d14de7146594e443d75aa84cb10463
nowshad99 <nowshad.ahmed@sainsburys.co.uk>
2023-03-30 12:34:54 +0100

    feat: add paddles and centre line

--- script.js
+++ script.js
@@ -6,11 +6,34 @@ const context = canvas.getContext("2d");
 const width = 500;
 const height = 700;
 
+// Paddle
+const paddleHeight = 10;
+const paddleWidth = 50;
+let paddleBottomX = 225;
+let paddleTopX = 225;
+
 // Render everything on Canvas
 function renderCanvas() {
-    // Canvas Background
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, width, height);
+  // Canvas Background
+  context.fillStyle = "black";
+  context.fillRect(0, 0, width, height);
+
+  // Paddle Colour
+  context.fillStyle = "white";
+
+  // Player Paddle (Bottom)
+  context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
+
+  // Computer Paddle (Top)
+  context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
+
+  // Dashed Center Line
+  context.beginPath();
+  context.setLineDash([4]);
+  context.moveTo(0, 350);
+  context.lineTo(500, 350);
+  context.strokeStyle = 'grey';
+  context.stroke();
 }
 
 // Create Canvas Element
