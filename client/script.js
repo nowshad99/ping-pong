ac94d1b5daa0062717cffb843028c8f65d66a0c0
nowshad99 <nowshad.ahmed@sainsburys.co.uk>
2023-03-31 16:06:18 +0100

    feat: add pong namespace

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@
 const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
 
-const socket = io('/');
+const socket = io('/pong');
 
 let isReferee = false;
 let paddleIndex = 0;
