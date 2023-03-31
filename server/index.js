39ad4c618dfb5831e65ef6f5a10702eaa5d90050
nowshad99 <nowshad.ahmed@sainsburys.co.uk>
2023-03-31 16:03:02 +0100

    feat: add express server

--- server/index.js
+++ server/index.js
@@ -1,39 +1,19 @@
-const server = require("http").createServer();
-const io = require("socket.io")(server, {
+const http = require("http");
+const io = require("socket.io");
+const app = require("./app");
+
+const httpServer = http.createServer(app);
+const socketServer = io(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
-const PORT = 3000;
+const sockets = require("./sockets");
 
-server.listen(PORT);
+const PORT = 3000;
+httpServer.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
-let readyPlayerCount = 0;
-
-io.on("connection", (socket) => {
-  console.log("A user connected: ", socket.id);
-
-  socket.on("ready", () => {
-    console.log(`Player ${socket.id} is ready`);
-    readyPlayerCount++;
-
-    if (readyPlayerCount % 2 === 0) {
-      io.emit("startGame", socket.id);
-    }
-  });
-
-  socket.on("paddleMove", (paddleData) => {
-    socket.broadcast.emit("paddleMove", paddleData);
-  });
-
-  socket.on("ballMove", (ballData) => {
-    socket.broadcast.emit("ballMove", ballData);
-  });
-
-  socket.on("disconnect", (reason) => {
-    console.log(`Client ${socket.id} disconnected: ${reason}`);
-  });
-});
+sockets.listen(socketServer);
